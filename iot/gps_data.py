88df8b6fdda306fcb8bef21fc34e6307601d5e9d
Marvellous <akomolafemarvellous@gmail.com>
2024-02-22 21:29:13 +0100

    Asded the file for generating the GPS data

---
+++ iot/gps_data.py
@@ -0,0 +1,25 @@
+import uuid
+import random
+from logger.logger import logs
+
+
+class GPSData:
+
+    def __init__(self):
+
+        self.logger = logs(__name__)
+
+    def generate_gps_data(self, device_id, timestamp, vehicle_type='private'):
+
+        gps_data = {
+            'id': uuid.uuid4(),
+            'device_id': device_id,
+            'timestamp': timestamp,
+            'speed': random.uniform(0, 40),
+            'direction': 'North-East',
+            'vehicle_type': vehicle_type,
+        }
+
+        self.logger.info(gps_data)
+
+        return gps_data
