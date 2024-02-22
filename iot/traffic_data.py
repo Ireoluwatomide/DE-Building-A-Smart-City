2f260f0f2d37071fc3c1ba808c7c2e1b253e7b80
Marvellous <akomolafemarvellous@gmail.com>
2024-02-23 00:31:52 +0100

    Added the file for generating the traffice data

---
+++ iot/traffic_data.py
@@ -0,0 +1,25 @@
+import uuid
+
+from logger.logger import logs
+
+
+class TrafficData:
+
+    def __init__(self):
+
+        self.logger = logs(__name__)
+
+    def generate_traffic_camera_data(self, device_id, timestamp, location, camera_id):
+
+        traffic_camera_data = {
+            'id': uuid.uuid4(),
+            'device_id': device_id,
+            'camera_id': camera_id,
+            'location': location,
+            'timestamp': timestamp,
+            'snapshot': 'Base64EncodedString'
+        }
+
+        # self.logger.info(traffic_camera_data)
+
+        return traffic_camera_data
