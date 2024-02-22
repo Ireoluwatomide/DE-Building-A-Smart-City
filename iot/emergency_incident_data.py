8ec8bb3a36a55da2e0e601fd11d24c5bb45e75ba
Marvellous <akomolafemarvellous@gmail.com>
2024-02-23 00:31:16 +0100

    Asded the file for generating the emergency incident data

---
+++ iot/emergency_incident_data.py
@@ -0,0 +1,28 @@
+import uuid
+import random
+
+from logger.logger import logs
+
+
+class EmergencyIncidentData:
+
+    def __init__(self):
+
+        self.logger = logs(__name__)
+
+    def get_emergency_incident_data(self, device_id, timestamp, location):
+
+        emergency_incident_data = {
+            'id': uuid.uuid4(),
+            'device_id': device_id,
+            'incident_id': uuid.uuid4(),
+            'type': random.choice(['Accident', 'Fire', 'Medical', 'Police', 'None']),
+            'timestamp': timestamp,
+            'location': location,
+            'status': random.choice(['Active', 'Resolved']),
+            'description': 'Description of the incident'
+        }
+
+        # self.logger.info(emergency_incident_data)
+
+        return emergency_incident_data
